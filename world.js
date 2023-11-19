6375b7b6e762a4b7cea485c2fa20c3936848be1b
Tevin Hislop <tevin.hislop@mymona.uwi.edu>
2023-11-19 17:50:45 -0500

    Completed Exercise #3

---
+++ world.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    var lookupButton = document.getElementById('lookup');
+
+    lookupButton.addEventListener('click', function(){
+
+        var countryInput = document.getElementById('country').value;
+
+        var xhr = new XMLHttpRequest();
+
+        url = 'http://localhost/info2180-lab5/world.php?country=' + countryInput;
+        xhr.open('GET', url);
+
+        xhr.onload = function(){
+            if(xhr.status === 200){
+                // console.log(xhr.responseText);
+                var response = xhr.responseText;
+                document.getElementById('result').innerHTML = response;
+            }else{
+                console.log('Error: ' + xhr.status);
+            }
+        }
+        xhr.send();
+
+    });
+
+})
--- world.php
+++ world.php
@@ -1,17 +1,33 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+if (isset($_GET['country'])) {
+
+  $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+  $countryParam = '%' . $_GET['country'] . '%';
+  $stmt->bindParam(':country', $countryParam, PDO::PARAM_STR);
+  $stmt->execute();
+  
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+  if (!empty($results)) {
+      echo '<ul>';
+      foreach ($results as $row) {
+          echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+      }
+      echo '</ul>';
+  } else {
+      echo 'No results found for the specified country.';
+  }
+} 

635cf0890dd1c197432e9732e5a1341cfaa21e26
Tevin Hislop <tevin.hislop@mymona.uwi.edu>
2023-11-19 21:49:20 -0500

    Completed Exercise #5

--- world.js
+++ world.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     var lookupButton = document.getElementById('lookup');
+    var cityButton = document.getElementById('city-lookup');
 
     lookupButton.addEventListener('click', function(){
 
@@ -13,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
         xhr.onload = function(){
             if(xhr.status === 200){
-                // console.log(xhr.responseText);
+                console.log(xhr.responseText);
                 var response = xhr.responseText;
                 document.getElementById('result').innerHTML = response;
             }else{
@@ -24,4 +25,26 @@ document.addEventListener('DOMContentLoaded', function(){
 
     });
 
-})
+    cityButton.addEventListener('click', function(){
+
+        var countryInput = document.getElementById('country').value;
+
+        var xhr = new XMLHttpRequest();
+
+        url = 'http://localhost/info2180-lab5/world.php?country=' + countryInput + '&lookup=cities';
+        xhr.open('GET', url);
+
+        xhr.onload = function(){
+            if(xhr.status === 200){
+                console.log(xhr.responseText);
+                var response = xhr.responseText;
+                document.getElementById('result').innerHTML = response;
+            }else{
+                console.log('Error: ' + xhr.status);
+            }
+        }
+        xhr.send();
+
+    });
+
+});
--- world.php
+++ world.php
@@ -14,9 +14,61 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 
 if (isset($_GET['country'])) {
 
-  $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
-  $countryParam = '%' . $_GET['country'] . '%';
-  $stmt->bindParam(':country', $countryParam, PDO::PARAM_STR);
+  $countryInput = '%' . filter_var($_GET['country'], FILTER_SANITIZE_STRING) . '%';
+  $lookupType = 'countries';
+  
+  if (isset($_GET['lookup'])){
+    if ($_GET['lookup'] === 'cities'){
+      $lookupType = 'cities';
+    }
+  }
+
+  if ($lookupType === 'countries'){
+    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+
+    $stmt->bindParam(':country', $countryInput, PDO::PARAM_STR);
+    $stmt->execute();
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    if (!empty($results)) {
+        echo '<table>';
+        echo '<tr><th>Name</th><th>Continent</th><th>Independence</th><th>Head of State</th></tr>';
+        foreach ($results as $row) {
+            echo '<tr><td>' . $row['name'] . '</td><td>' . $row['continent'] . '</td><td>' . $row['independence_year'] . '</td><td>' . $row['head_of_state'] . '</td></tr>';
+        }
+        echo '</table>';
+    } else {
+        echo 'No results found for the specified country.';
+    }
+  }
+
+  elseif ($lookupType === 'cities') {
+    $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE :country");
+
+    $stmt->bindParam(':country', $countryInput, PDO::PARAM_STR);
+    $stmt->execute();
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (!empty($results)) {
+      echo '<table>';
+      echo '<tr><th>Name</th><th>District</th><th>Population</th></tr>';
+      foreach ($results as $row) {
+          echo '<tr><td>' . $row['name'] . '</td><td>' . $row['district'] . '</td><td>' . $row['population'] . '</td></tr>';
+      }
+      echo '</table>';
+  } else {
+      echo 'No results found for the specified country.';
+    }
+
+  }
+
+} 
+
+
+  /* $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+  $stmt->bindParam(':country', $countryInput, PDO::PARAM_STR);
   $stmt->execute();
   
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -30,5 +82,4 @@ if (isset($_GET['country'])) {
       echo '</table>';
   } else {
       echo 'No results found for the specified country.';
-  }
-} 
+  } */
